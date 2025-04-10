2d8f86618949c59c7d94246b47e60ce3bacecec0
Jeremiah <2240521@slu.edu.ph>
2025-04-10 22:49:04 +0800

    Created the setters and getters in GameRoomPageView, created the initial structures in GameRoomPageController and GameRoomPageModel

--- src/main/java/Client_Java/controller/GameRoomPageController.java
+++ src/main/java/Client_Java/controller/GameRoomPageController.java
@@ -1,4 +1,32 @@
 package Client_Java.controller;
 
+import Client_Java.model.GameRoomPageModel;
+import Client_Java.view.GameRoomPageView;
+
 public class GameRoomPageController {
+    private final GameRoomPageView view;
+    private final GameRoomPageModel model;
+
+    public GameRoomPageController(GameRoomPageView view, GameRoomPageModel model) {
+        this.view = view;
+        this.model = model;
+        initialize();
+    }
+
+    private void initialize() {
+        // Bind model properties to the view
+        view.getLifeCountLabel().textProperty().bind(model.lifeCountProperty().asString());
+
+//TODO:   Set up event handlers
+//        view.getBlank().setOnMouseClicked(event -> handleBlankClick('A')); // Example: Guess the letter 'A'
+//        view.getBlank1().setOnMouseClicked(event -> handleBlankClick('B')); // Example: Guess the letter 'B'
+    }
+
+    private void handleBlankClick(char letter) {
+        model.guessLetter(letter);
+    }
+
+    public void handleReturnToHome() {
+        model.returnToHome();
+    }
 }
--- src/main/java/Client_Java/model/GameRoomPageModel.java
+++ src/main/java/Client_Java/model/GameRoomPageModel.java
@@ -1,4 +1,54 @@
 package Client_Java.model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class GameRoomPageModel {
+    private IntegerProperty lifeCount = new SimpleIntegerProperty(5);
+    private String currentWord;
+    private int roundCount = 0;
+    private int playerWins = 0;
+    private int opponentWins = 0;
+
+    public IntegerProperty lifeCountProperty() {
+        return lifeCount;
+    }
+
+    public void guessLetter(char letter) {
+        //TODO: Logic for guessing a letter
+        // Update UI and check if the letter is correct
+    }
+
+    public void submitWord(String word) {
+        //TODO: Logic for submitting a word
+        // Check if the word matches the currentWord
+    }
+
+    public void returnToHome() {
+        //TODO: Logic to return to the home screen
+    }
+
+    public void startNewRound() {
+        //TODO: Logic to start a new round
+        // Select a new word and reset attempts
+    }
+
+    public void endRound(boolean playerWon) {
+        // Logic to end the round
+        if (playerWon) {
+            playerWins++;
+        } else {
+            opponentWins++;
+        }
+        if (playerWins == 3 || opponentWins == 3) {
+            endGame();
+        } else {
+            startNewRound();
+        }
+    }
+
+    private void endGame() {
+        //TODO: Logic to end the game
+        // Return to the home screen
+    }
 }
