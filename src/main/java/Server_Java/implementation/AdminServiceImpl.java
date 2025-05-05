20981c9c50ee95eada6450554e9ac16fd9e00f4d
Giana <2241883@slu.edu.ph>
2025-05-06 00:57:43 +0800

    created functionality to the admin create player and modify game configuration

--- src/main/java/Server_Java/implementation/AdminServiceImpl.java
+++ src/main/java/Server_Java/implementation/AdminServiceImpl.java
@@ -1,12 +1,58 @@
 package Server_Java.implementation;
 
+import Server_Java.database.DatabaseConnection;
 import Server_Java.idls.AdminIDL.*;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
 public class AdminServiceImpl extends AdminServicePOA {
+    private static String query; // holds the sql query
+    private final AuthenticationServiceImpl authService;
+
+    public AdminServiceImpl(AuthenticationServiceImpl authService) {
+        this.authService = authService;
+    }
+
+    public static int generatePlayerID() {
+        Random random = new Random();
+        int id = random.nextInt(90000) + 10000; // Generates a number between 10000 and 99999
+        return id;
+        // TODO - check existing player id to prevent duplication
+    }
 
     @Override
     public void createPlayer(String username, String password, String sessionToken, int adminID) throws AccountExistsException, NotLoggedInException {
+        // FIXME - add string fullname as parameter also in idl
+        query ="INSERT INTO players (player_id, name, username, password) " +
+                "VALUES (?, ?, ?, ?); ";
 
+        try (Connection con = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = con.prepareStatement(query)){
+            int player_id = generatePlayerID();
+            String fullName = "Place holder"; //temporary statement bago ma-recompile and idl
+            preparedStatement.setInt(1, player_id);
+            preparedStatement.setString(2, fullName);
+            preparedStatement.setString(3, username);
+            preparedStatement.setString(4, password);
+
+            preparedStatement.executeUpdate();
+            // Get the current timestamp and print the action
+            LocalDateTime timestamp = LocalDateTime.now();
+            String formattedTimestamp = timestamp.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+            // Print the action details
+            String adminUsername = authService.getAdminUsernameByToken(sessionToken);
+            System.out.println("[" + formattedTimestamp +"] [Admin: " + adminUsername + "] - Action: Player Created  - Details: Player ID: " + player_id +", Username: " + username);
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }catch (Exception e1){
+            e1.printStackTrace();
+        }
     }
 
     @Override
@@ -26,11 +72,27 @@ public class AdminServiceImpl extends AdminServicePOA {
 
     @Override
     public void modifyWaitingTime(int waitTime, String sessionToken, int adminID) throws NotLoggedInException {
-
+        String query = "UPDATE settings SET waiting_time = ?";
+        try (Connection con = DatabaseConnection.getConnection()) {
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.setInt(1, waitTime);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
     }
 
     @Override
     public void modifyRoundDuration(int roundTime, String sessionToken, int adminID) throws NotLoggedInException {
-
+        String query = "UPDATE settings SET round_duration = ?";
+        try (Connection con = DatabaseConnection.getConnection()) {
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.setInt(1, roundTime);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
     }
 }
